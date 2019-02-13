5990311da462e3b8cb1335bf32819bd1af4c631e
Uwe B <kontakt@uwe-barthel.net>
2019-02-13 10:07:36 +0100

    Update Gridsome Setup

---
+++ gridsome.config.js
@@ -0,0 +1,25 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Stadt Augustusburg',
+  plugins: [
+    /* {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: 'WEBSITE_URL', // required
+        apiBase: 'wp-json',
+        typeName: 'WordPress',
+        perPage: 100,
+        concurrent: 10,
+        routes: {
+          post: '/:year/:month/:day/:slug',
+          post_tag: '/tag/:slug'
+        }
+      }
+    }*/
+  ]
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default {
+  name: "Home",
+  metaInfo: {
+    title: "Home",
+    meta: [{ name: "author", content: "John Doe" }],
+    link: [{ rel: "stylesheet", href: "/css/index.css" }]
+    // etc...
+  }
+};
+</script>
